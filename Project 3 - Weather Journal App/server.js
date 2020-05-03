d7cb4a68975883edc34a195fdfabf1d0de5f9420
yuqiliao <yuqiliao@hotmail.com>
2020-05-03 17:54:25 -0400

    Project 3 - complete

--- Project 3 - Weather Journal App/server.js
+++ Project 3 - Weather Journal App/server.js
@@ -2,8 +2,13 @@
 projectData = {};
 
 // Require Express to run server and routes
+const express = require("express");
 
 // Start up an instance of app
+const app = express();
+
+/*Dependencies*/
+const bodyParser = require("body-parser");
 
 /* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
@@ -11,9 +16,42 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Cors for cross origin allowance
+const cors = require("cors");
+app.use(cors());
 
 // Initialize the main project folder
 app.use(express.static('website'));
 
+const port = 3000;
 
 // Setup Server
+const server = app.listen(port, () => {
+    console.log(`this is working! the server is running on localhost: ${port}`)
+});
+
+// GET method route
+app.get("/database", function(req, res){
+    // console.log(req);
+    res.send(projectData);
+});
+
+// POST method route
+
+app.post("/addData", function(req, res){
+
+    projectData['temperature'] = req.body.temperature;
+    projectData['date'] = req.body.date;
+    projectData['userFeeling'] = req.body.userFeeling;
+
+    res.send(projectData);
+
+    //res.send("POST received");
+    console.log(projectData);
+    // data.push(req.body);
+    // console.log(data);
+});
+
+
+
+
+
--- Project 3 - Weather Journal App/website/app.js
+++ Project 3 - Weather Journal App/website/app.js
@@ -1,5 +1,91 @@
 /* Global Variables */
+let baseURL = 'http://api.openweathermap.org/data/2.5/weather?zip='
+let apiKey = '1802912abfaa1ea29ddcf5d66c39e5e0';
+//api documentation: https://openweathermap.org/current
 
 // Create a new date instance dynamically with JS
 let d = new Date();
-let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+let newDate = d.getMonth() + 1 +'/'+ d.getDate()+'/'+ d.getFullYear();
+// console.log(d);
+// console.log(newDate);
+
+
+/* Function to GET data from OpenWeather API */
+const getWeatherData = async (baseURL, zipCode, apiKey) => {
+    const res = await fetch(baseURL+zipCode+",us&units=imperial"+"&appid="+apiKey);
+    // console.log(baseURL+zipCode+",us"+"&appid="+apiKey);
+    // console.log(res);
+    try {
+        const data = await res.json();
+        //console.log(data);
+        return data.main.temp;
+    } catch (error) {
+        console.log("error:", error);
+    }
+}
+
+
+
+
+/* Function to POST combined data to server */
+const postData = async ( url = '', data = {})=>{
+    //console.log(data)
+      const res = await fetch(url, {
+      method: 'POST', // *GET, POST, PUT, DELETE, etc.
+      credentials: 'same-origin', // include, *same-origin, omit
+      headers: {
+          'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data), // body data type must match "Content-Type" header        
+    });
+      try {
+        //console.log(res);
+        const newData = await res.json();
+        //console.log(newData);
+        return newData
+      }catch(error) {
+      console.log("error", error); 
+      }
+  }
+
+
+/* Function to GET combined data from server & update DOM*/
+const updateDOM = async () => {
+    
+    const req = await fetch('/database');
+    try {
+        //console.log(req);
+        const allData = await req.json();
+        //console.log(allData);
+        
+        //update values with new entry
+        document.querySelector('#userDate').innerHTML = allData.date;
+        document.querySelector('#userTemp').innerHTML = allData.temperature;
+        document.querySelector('#userFeeling').innerHTML = allData.userFeeling;
+    
+    } catch(error) {
+
+        console.log('error', error);
+    }
+};
+
+// Add event listener
+document.querySelector("#generate").addEventListener('click', getResults);
+
+function getResults(event) {
+
+    //event.preventDefault();
+
+    // get user input
+    const userZipCode = document.querySelector('#zip').value;
+    const userFeeling = document.querySelector('#feelings').value;
+
+    //get weather data
+    getWeatherData(baseURL, userZipCode, apiKey)
+        //combine weather data & other data; post to server
+        .then(function(data) {
+            postData("/addData", {temperature: data, date: newDate, userFeeling: userFeeling});
+        })
+        //get data from server & update dom 
+        .then(updateDOM);
+}
