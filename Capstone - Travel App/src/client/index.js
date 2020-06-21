90b805125256e6992b6d1b0d85d5b536365b4d74
yuqiliao <yuqiliao@hotmail.com>
2020-06-21 12:12:47 -0400

    add test

--- Capstone - Travel App/src/client/index.js
+++ Capstone - Travel App/src/client/index.js
@@ -4,6 +4,18 @@ import { removeResults } from './js/app'
 
 import './styles/style.scss'
 
+
+// // try to import images from the client folder to dist folder but failed :(
+// function importAll(r) {
+//     return r.keys().map(r);
+//   }
+
+// const images = importAll(require.context('./', false, /\.(png|jpe?g|svg)$/));
+
+// var icon = document.getElementById("tempIcon")
+// icon.src = images;
+
+
 export {
     displayResults,
     removeResults
--- Capstone - Travel App/src/client/js/test.js
+++ Capstone - Travel App/src/client/js/test.js
@@ -1,13 +1,22 @@
-describe("default city picture", () => {
+// get user input of cityName
+function checkUserCityName(cityName){
+    let userCityNameEntered = true;
+
+    if (cityName.length === 0){
+        userCityNameEntered = false;
+    } else {
+        userCityNameEntered = true;
+    }
+
+    return userCityNameEntered;
+}
+
+describe("check user city name input", () => {
 
     test('user has NOT entered the city name', () => {
-        // get user input of cityName
-        const userCityName = document.querySelector('#city').value;
-        if(userCityName === ""){
-            //if there's no user input, the default picture should be loaded
-            const cityPicURL = document.querySelector('#cityPic').innerHTML
-            expect(cityPicURL).toStrictEqual('<img src="https://cdn.pixabay.com/photo/2017/01/16/15/51/hiker-1984421_960_720.jpg" alt="destination city picture">');
-        } 
-      
+        expect(checkUserCityName("")).toBeFalsy();    
+    });
+    test('user has entered the city name', () => {
+        expect(checkUserCityName("New York")).toBeTruthy();    
     });
   })
